7c703e55159e496f6ed06d41fda8ca74c8b7dec7
meyer.hugo <meyer.hugo@06887c25-314c-0410-aad7-7fa44b44e222>
2008-06-02 01:43:24 +0000

    Versión final, falta cambiar si queremos usar malloc, y ademas si queremos leer de un archivo

--- trunk/mm1d.c
+++ trunk/mm1d.c
@@ -1,29 +1,36 @@
-/**
- * File Name: sam_mm.c
- * Sample matrix multiplication code.
- * Algorithm: A and C are partitioned in 1D row blocks, B is partitioned in 
- * 1D column blocks. 
+
+/*********************************************************************************************
+ * Archivo: mm1d.c
  *
- * The main multiplication involves bnum iterations, 
- * in iteration i, the ith sub-matrix column of B is broadcast to everybody 
- * then everybody uses this column and its own portion of A to update the ith 
- * column of its own portion of C.
+ * Multiplicacion de matrices utilizando hilos y el particionamiento 1-d
  *
- * To compile:
- * cc sam_mm.c -o sam_mm -lmpi
  *
- * Comments: I've put lots of effort to make the code as easy to read as 
- * possible. So many opportunities for optimiaztions are possible, e.g.
- * all the dynmic checking of ownership can be eliminated, and even the
- * memcpy(...) can be removed.
+ * Para compilar:
+ * cc mm1d.c -o mm1d -lpthread
  *
- * -Hong Tang, May. 1999
- */
+ * Para ejecutar:
+ * ./mm1d <cantidad_de_hilos>
+ *
+ * Adaptacion para su implementacion con hilos, particionamiento 1-D y matrices de tamanhos
+ * arbitrarias (multiplicables) por:
+ *
+ * - Eduardo Rivas. erivas17@gmail.com
+ * - Hugo Meyer. meyer.hugo@gmail.com
+ *
+ * Alumnos de la Universidad Nacional de Asuncion - Facultad Politecnica.
+ * Carrera: Ingenieria Informatica.
+ * Materia: Electiva V - Algoritmos Paralelos.
+ * Profesor: Cristian Von Lucken.
+ * Anho: 2008.
+ **********************************************************************************************/
 
 # include <stdio.h>
 # include <stdlib.h>
 # include <pthread.h>
 # include <sys/time.h>
+# define filasA 4		/* Cantidad de filas de la matriz A */
+# define columnasB 5		/* Cantidad de columnas de la matriz A = Cantidad de filas de la matriz B */
+# define comunAB 3		/* Cantidad de columnas de la matriz B */
 
 static int n;	 	 // tamaño de la matriz
 static int bsize;	 //tamaño de la submatriz
@@ -32,14 +39,10 @@ static int subnum;	 //numero de submatrices.
 static int resto;
 static pthread_mutex_t lock;
 static int tarea = -1; 	// indica el id de la tarea actual 
-int filasA;
-int columnasB;
-int comunAB;
+static int cont =0;
 
-double *A;
-double *B;
-double *C;
 
+static int A[filasA][comunAB], B[comunAB][columnasB], C[filasA][columnasB];
 struct indices               /* Tipo de dato para representar */
 {	int i, j;          		/* los ndices de una matriz */
 };
@@ -47,12 +50,12 @@ typedef struct indices indice;
 indice indiceGral;/* Aqui guardamos los indices de la matriz C para saber en que posición de ella estamos en un momento dado y nos
 				sirve para movernos a traves de ella.*/
 
-void cargarMatriz(double *, int, int);
+void cargarMatrices();
 void realizarTarea();
 void *mapearTarea(void *);
 void getIndice(int);
 double getTime();
-void imprimirMatriz(double *, int, int);
+void imprimirMatrices();
 
 int main(int argc, char *argv[]){
 	
@@ -60,9 +63,9 @@ int main(int argc, char *argv[]){
 	pthread_t *hilos;
 	double t1, t2;
 	/*Para probar definimos arbitrariamente una matriz los tamaños de la matriz*/
-	filasA=4;
-	columnasB=5;
-	comunAB = 3;
+	//filasA=4;
+	//columnasB=5;
+	//comunAB = 3;
 	
 	/* Inicializacion de parametros de la matriz/submatriz */
 	if (argc!=2) {
@@ -80,27 +83,25 @@ int main(int argc, char *argv[]){
 	hilos = (pthread_t *)malloc(sizeof(pthread_t) * (cant_hilos-1));
 	pthread_mutex_init(&lock, NULL);
 	//Inicialización de las matrices.
-	A = (double *)malloc (filasA*comunAB* sizeof(double));
-	B = (double *)malloc (columnasB*comunAB* sizeof(double));
-	C = (double *)malloc (filasA*columnasB* sizeof(double ));
-	cargarMatriz(A, filasA ,comunAB);
-	cargarMatriz(B,comunAB,columnasB);
+	//A = (double *)malloc (filasA*comunAB* sizeof(double));
+	//B = (double *)malloc (columnasB*comunAB* sizeof(double));
+	//C = (double *)malloc (filasA*columnasB* sizeof(double ));
+	cargarMatrices();
 	for (i=0; i < filasA; i++){
 		for (j=0; j < columnasB; j++) {
-			C[i*filasA+j] = 0;
+			C[i][j] = 0;
 		}
 	}
 	
-	printf("Imprimimos matrices...\n");
-	printf("Matriz A\n");
-	imprimirMatriz(A,filasA,comunAB);
-	printf("Matriz B\n");
-	imprimirMatriz(B,comunAB,columnasB);
 	
+	imprimirMatrices();
 	
 	printf("Ok!!\n\n");
 	printf("Multiplicacion ...\n");
 	pthread_setconcurrency(cant_hilos);
+	/*ESTO LO HACEMOS PARA QUE LA PRIMERA VEZ QUE ENTRE A LLAMAR A GETINDICE, SE TOME
+	EL INDICE 0*/
+	indiceGral.j = columnasB -1;
 	
 	t1=getTime();
 	//creamos un hilo por cada proceso y le mandamos su parte para multiplicar
@@ -113,22 +114,31 @@ int main(int argc, char *argv[]){
 
 	t2=getTime();
 	//AK HAY ERROR
-	printf("Matriz C\n");
-	imprimirMatriz(C,filasA,columnasB);
+	imprimirMatrices();
 	printf("Duracion total de la multilplicacion de matrices %4f segundos\n", t2-t1);
-	free(A);
-	free(B);
-	free(C);
+	printf("CANTIDAD DE VECES QUE LLAMAMOS A GETINDICE %d \n", cont);
+	printf("VALOR FINAL DE INDICEGRAL.J %d \n", indiceGral.j);
+	
 	
 }
-void cargarMatriz(double X[], int fila, int columna){
+void cargarMatrices(){
 	int i =0;
 	int j = 0;
-	printf(" Filas %d y columna %d \n", fila, columna);
-	for (i=0; i < fila; i++){
-		for (j=0; j < columna; j++) {
-			X[i*fila+j]=(rand()%100)+1;
-			printf(" Valor %2f \n",X[i*fila+j] );
+	printf(" Filas %d y columna %d \n", filasA, comunAB);
+
+	for (i=0; i < filasA; i++){
+		for (j=0; j < comunAB; j++) {
+			A[i][j]=(rand()%100)+1;
+			printf(" Valor %d \n",A[i][j]);
+			//printf("%2d",X[i*fila+j]);
+		}
+	}
+	printf(" Filas %d y columna %d \n", comunAB, columnasB);
+
+	for (i=0; i < comunAB; i++){
+		for (j=0; j < columnasB; j++) {
+			B[i][j]=(rand()%100)+1;
+			printf(" Valor %d \n",B[i][j]);
 			//printf("%2d",X[i*fila+j]);
 		}
 	}	
@@ -156,18 +166,24 @@ void realizarTarea()
 	//printf("i-j: %d.-.%d\n", indi.i, indi.j);
 	// CORREGIMOS COLUMNASB POR COMUNAB
 	for (k=0; k < comunAB; k++) {
-		C[indiceGral.i*columnasB +indiceGral.j] += A[indiceGral.i*comunAB+k] * B[columnasB*k + indiceGral.j];
-		printf("VALORES MULTIPLICADOS %2f * %2f\n", A[indiceGral.i*comunAB+k], B[columnasB*k + indiceGral.j]);
-		printf("VALOR DE LA MATRIZ PARCIAL C[%d] -> %2f \n", indiceGral.i*columnasB +indiceGral.j, C[indiceGral.i*columnasB +indiceGral.j]);
+		pthread_mutex_lock(&lock);
+		C[indiceGral.i][indiceGral.j] += A[indiceGral.i][k] * B[k][indiceGral.j];
+		printf("VALORES MULTIPLICADOS %d * %d\n", A[indiceGral.i][k], B[k][indiceGral.j]);
+		printf("VALOR DE LA MATRIZ PARCIAL C[%d][%d] -> %d \n", indiceGral.i, indiceGral.j, C[indiceGral.i][indiceGral.j]);
+		pthread_mutex_unlock(&lock);
 	}
-	printf("VALOR FINAL DEL CAMPO C[%d] -> %2f \n", indiceGral.i*columnasB +indiceGral.j, C[indiceGral.i*columnasB +indiceGral.j]);
+	printf("VALOR FINAL DEL CAMPO C[%d][%d] -> %d \n", indiceGral.i,indiceGral.j, C[indiceGral.i][indiceGral.j]);
 	//sumarSubMatriz(indiceA, indiceB, indiceC);
 	//indiceA.j += SUBN;
 	//indiceB.i += SUBN;
 	//Incrementamos aqui el valor para que la siguiente tarea, se mueva un lugar en la matriz C.
 	//printf("indice de la tarea J %d \n", indiceGral.j);
-	indiceGral.j =indiceGral.j +1;
-
+	//pthread_mutex_lock(&lock);
+	//indiceGral.j =indiceGral.j +1;
+	//pthread_mutex_unlock(&lock);
+	printf("INDICE DE LA TAREA I %d \n", indiceGral.i);
+	printf("INDICE DE LA TAREA J %d \n", indiceGral.j);
+	
 }
 
 
@@ -192,8 +208,16 @@ void *mapearTarea(void *arg)
 	}
 }
 void getIndice(int tarea)
-{
+{	
+	cont++;
+	pthread_mutex_lock(&lock);
 	indiceGral.i = (int)tarea%subnum;
+	indiceGral.j += 1;
+	if(indiceGral.j == columnasB){
+		indiceGral.j = 0;
+	}
+	pthread_mutex_unlock(&lock);
+	
 	//return indi;
 }
 double getTime() {
@@ -202,15 +226,43 @@ double getTime() {
 	return (double)t.tv_sec+t.tv_usec*0.000001;
 }
 
-void imprimirMatriz(double m[], int fil, int col)
+void imprimirMatrices()
 {
 	int i, j = 0;
-	for (i=0; i < fil; i++) {
+	printf("Imprimimos matrices...\n");
+	printf("Matriz A\n");
+
+	for (i=0; i < filasA; i++) {
+
 		printf("\n\t| ");
-		for (j=0; j < col; j++)
-			printf("%2f ", m[i*fil + j]);
+		for (j=0; j < comunAB; j++)
+			printf("%d ", A[i][j]);
 		printf("|");
 	}
+
 	printf("\n");
+	printf("Matriz B\n");
+
+	for (i=0; i < comunAB; i++) {
+
+		printf("\n\t| ");
+		for (j=0; j < columnasB; j++)
+			printf("%d ", B[i][j]);
+		printf("|");
+	}
+
+	printf("\n");
+	printf("Matriz C\n");
+
+	for (i=0; i < filasA; i++) {
+
+		printf("\n\t| ");
+		for (j=0; j < columnasB; j++)
+			printf("%d ", C[i][j]);
+		printf("|");
+	}
+
+	printf("\n");
+	
 }
 
